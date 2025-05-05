8d9815108ef0cdf7f56573890034b1c744ee8a0e
Mike Bruce <mike@hypalynx.com>
2025-05-06 00:25:18 +0100

    include indirect/direct partial test case

--- keystone_test.go
+++ keystone_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"path/filepath"
 	"testing"
+	"testing/fstest"
 
 	"github.com/hypalynx/keystone"
 	"github.com/hypalynx/keystone/testdata"
@@ -67,7 +68,48 @@ func (s *KeystoneTestSuite) TestTemplateRender() {
 	s.Equal(string(expected), dst.String())
 }
 
-// Test Render on disk change.. good luck writing that test!
+// This test recreates an issue I ran into where I could use a partial inside a page template but not directly.
+func TestFullPathTemplateRendering(t *testing.T) {
+	// Create an in-memory filesystem for testing
+	fsys := fstest.MapFS{
+		"partials/test-partial.tmpl": &fstest.MapFile{
+			Data: []byte(`{{ define "partials/test-partial.tmpl" }}<div class="partial">Hello, {{ .Name }}!</div>{{ end }}`),
+		},
+		"pages/test-parent.tmpl": &fstest.MapFile{
+			Data: []byte(`
+<!DOCTYPE html>
+<html>
+<body>
+    <div id="container">
+        {{ template "partials/test-partial.tmpl" . }}
+    </div>
+</body>
+</html>
+`),
+		},
+	}
+
+	registry, err := keystone.New(fsys)
+	require.NoError(t, err)
+
+	registry.Debug = true
+
+	// Render parent template (initial page load scenario)
+	var parentBuf bytes.Buffer
+	err = registry.Render(&parentBuf, "pages/test-parent.tmpl", map[string]any{
+		"Name": "World",
+	})
+	require.NoError(t, err, "Should render parent template successfully")
+	require.Contains(t, parentBuf.String(), "Hello, World!", "Parent template should contain rendered partial")
+
+	// Render just the partial (HTMX partial load scenario)
+	var partialBuf bytes.Buffer
+	err = registry.Render(&partialBuf, "partials/test-partial.tmpl", map[string]any{
+		"Name": "HTMX",
+	})
+	require.NoError(t, err, "Should render partial template successfully")
+	require.Contains(t, partialBuf.String(), "Hello, HTMX!", "Partial should render correctly")
+}
 
 func TestKeystoneSuite(t *testing.T) {
 	suite.Run(t, new(KeystoneTestSuite))
